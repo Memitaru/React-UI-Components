c22cb81e6f9f4846ef09ef86c78cf50f0a408175
Memitaru <amandatwah@gmail.com>
2019-02-27 18:36:08 -0800

    Finished header and started on card components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './Header.css';
 
 const ImageThumbnail = () => {
-    return <img src="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png" />;
+    return (
+    <div>
+    <img src="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png" alt="Lambda school logo" className="logo" />
+    </div>
+    )
 }
 
 export default ImageThumbnail;
