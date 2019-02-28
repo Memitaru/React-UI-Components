01b8dbdb7e67a9aad3df2e55fe04a16f328e9ccf
Memitaru <amandatwah@gmail.com>
2019-02-27 19:33:09 -0800

    Finished footer

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+    return (
+        <footer>
+            <img src="https://img.icons8.com/ios/20/000000/speech-bubble.png" />
+            <img src="https://img.icons8.com/small/20/000000/retweet.png" />
+            <img src="https://img.icons8.com/ios/20/000000/hearts.png" />
+            <img src="https://img.icons8.com/ios/20/000000/new-post.png" />
+        </footer>
+    )
+}
+
+export default Footer;
