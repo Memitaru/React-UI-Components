0079aaac0f6d5b91b295c4853a609de8fd6ef75e
Memitaru <amandatwah@gmail.com>
2019-02-27 19:08:46 -0800

    Completed card components

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Card.css';
 import CardBanner from "./CardBanner";
+import CardContent from "./CardContent";
 
 const CardContainer = () => {
     return (
-        <div className="card-container">
-            <CardBanner />
-        </div>
+        <a href="https://www.reactjs.org">
+            <div className="card-container">
+                <CardBanner />
+                <CardContent />
+            </div>
+        </a>
     )
 }
 
